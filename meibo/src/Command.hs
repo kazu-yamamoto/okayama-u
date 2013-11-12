3fea2bd877e1d383766a47bc9f9141e69bab16ca
Kazu Yamamoto <kazu@iij.ad.jp>
2013-11-12 20:36:50 +0900

    adding %S.

--- meibo/src/Command.hs
+++ meibo/src/Command.hs
@@ -3,7 +3,9 @@ module Command (Command(..), command) where
 import Control.Applicative ((<$>))
 import Control.Exception
 import qualified Control.Exception as E
+import Data.Function (on)
 import Data.IORef
+import Data.List (sortBy)
 import System.Exit (exitSuccess)
 
 import CSVParser
@@ -19,6 +21,7 @@ eval _    Quit       = exitSuccess
 eval ref (Read file) = comRead ref file
 eval ref Check       = comCheck ref
 eval ref (Print n)   = comPrint ref n
+eval ref (Sort n)    = comSort ref n
 eval _   _           = return $ NG "not implemented"
 
 comRead :: IORef [Person] -> FilePath -> IO Result
@@ -40,5 +43,22 @@ comCheck ref = do
 comPrint :: IORef [Person] -> Int -> IO Result
 comPrint ref n = do
     db <- readIORef ref
-    mapM print $ take n db
+    let db' | n == 0    = db
+            | n >  0    = take n db
+            | otherwise = error "FIXME"
+    mapM print db'
     return OK
+
+comSort :: IORef [Person] -> Int -> IO Result
+comSort ref n = do
+    modifyIORef ref (sortBy (compareEntry n))
+    return OK
+
+-- FIXME: is N 0-origin?
+compareEntry :: Int-> Person -> Person -> Ordering
+compareEntry 0 = compare `on` personId
+compareEntry 1 = compare `on` personName
+compareEntry 2 = compare `on` personBirthday
+compareEntry 3 = compare `on` personAddress
+compareEntry 4 = compare `on` personMisc
+compareEntry _ = error "no such item"
